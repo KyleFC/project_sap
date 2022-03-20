594688cb8156aa4652aaca7c370b98714e4f73ec
Kyle Christenson <kwatonna@gmail.com>
2022-03-20 03:20:39 -0700

    Created game class to handle all click (rolls, moving, end turn, etc.)

--- SAP_CV.py
+++ SAP_CV.py
@@ -43,6 +43,83 @@ class Shop:
                 self.shop_dict[f'slot_{i}'] = identify_food(tl_coords)
         print('shop has been updated')
 
+class Team:
+    #team_dict is a dictionary of strings representing animals but should be a
+    #dictionary of pet classes in the future
+    def __init__(self, team_dictionary, team_coordinates):
+        self.team_dict = team_dictionary
+        #maybe add the +50 to x and y here eventually if i know i'm not doing any cv stuff here
+        self.team_coords = team_coordinates
+
+    def print_team(self):
+        """
+        print a formatted representation of the animals on the team.
+        """
+        print("{}|{}|{}|{}|{}".format(*self.team_dict.values()))
+
+class Game:
+
+    def __init__(self, shop, team, gold, round, hearts):
+        self.shop = shop
+        self.team = team
+        self.gold = gold
+        self.round = round
+        self.hearts = hearts
+
+    def move(self, slot1, slot2, click=False):
+        """
+        move a pet from slot_? to slot_?
+        if the pets are the same then it will combine
+        """
+        slot_coords_1 = self.shop.team_coords[slot1][0] + 50, self.shop.team_coords[slot1][1] + 50
+        slot_coords_2 = self.shop.team_coords[slot2][0] +50, self.shop.team_coords[slot2][1] + 50
+        dist = slot1 - slot2
+        if click:
+            pag.moveTo(slot_coords_1)
+            time.sleep(.1)
+            pag.click()
+            time.sleep(.5)
+            pag.moveTo(slot_coords_2)
+            time.sleep(.1)
+            pag.click()
+
+        #inner team logic
+        pet = self.team.team_dict[f'slot_{slot1}']
+        print(f'moving {slot1} to {slot2}')
+        self.team.team_dict[f'slot_{slot1}'] = None
+
+        if self.team.team_dict[f'slot_{slot2}'] == None or abs(dist) == 1:
+            self.team.team_dict[f'slot_{slot1}'] = self.team.team_dict[f'slot_{slot2}']
+            self.team.team_dict[f'slot_{slot2}'] = pet
+
+        #move from right to left. pets get pushed forward from slot
+        elif dist > 0:
+            for i in range(dist):
+                self.team.team_dict[f'slot_{slot1-i}'] = self.team.team_dict[f'slot_{slot1-i-1}']
+                #print(f'pushing {slot1-i-1} to {slot1-i}')
+            self.team.team_dict[f'slot_{slot2}'] = pet
+
+        #move from left to right. pets get pushed backwards from slot
+        else:
+            for i in range(-dist):
+                self.team.team_dict[f'slot_{slot1+i}'] = self.team.team_dict[f'slot_{slot1+i+1}']
+                #print(f'pushing {slot1+i} to {slot1+i+1}')
+            self.team.team_dict[f'slot_{slot2}'] = pet
+
+    def buy(self, item, shop_slot, team_slot):
+        """
+        buys the pet or food in shop_slot __. 0 - 6
+        puts the pet/food on team_slot
+        adds the animal
+        """
+        pag.moveTo(self.shop_coords[shop_slot][0] + 50, self.shop_coords[shop_slot][1] + 50)
+        time.sleep(.1)
+        pag.click()
+        time.sleep(.5)
+        pag.moveTo(self.team_coords[team_slot][0] + 50, self.team_coords[team_slot][1] + 50)
+        time.sleep(.5)
+        pag.click()
+        
     def roll(self):
         """
         Reroll the shop
@@ -59,18 +136,9 @@ class Shop:
         time.sleep(.1)
         pag.click()
 
-    def buy(self, shop_slot, team_slot):
-        """
-        buys the pet or food in shop_slot __. 0 - 6
-        puts the pet/food on team_slot
-        """
-        pag.moveTo(self.shop_coords[shop_slot][0] + 50, self.shop_coords[shop_slot][1] + 50)
-        time.sleep(.1)
-        pag.click()
-        time.sleep(.5)
-        pag.moveTo(self.team_coords[team_slot][0] + 50, self.team_coords[team_slot][1] + 50)
-        time.sleep(.5)
-        pag.click()
+    def print_game(self):
+        self.shop.print_shop()
+        self.team.print_team()
 
 def identify_pet(top_left):
     """
@@ -125,8 +193,11 @@ if __name__ == '__main__':
 
 
 ######################TEST ZONE#################################
-    my_shop = Shop(slot_coords, team_coords)
-    time.sleep(2)
+    shop_dict = {'slot_0': 'pig', 'slot_1': 'ant', 'slot_2': 'duck', 'slot_3': None, 'slot_4': None, 'slot_5': None, 'slot_6': 'apple'}
+    my_shop = Shop(slot_coords, team_coords, shop_dict)
+    team_dict = {'slot_0': 'pig', 'slot_1': 'ant', 'slot_2': 'duck', 'slot_3': 'swan', 'slot_4': 'fish'}
+    sample_team = Team(team_dict, team_coords)
+    """time.sleep(2)
     coords = my_shop.get_shop_coords()
     my_shop.update()
     my_shop.print_shop()
@@ -140,3 +211,8 @@ if __name__ == '__main__':
     my_shop.buy(0, 2)
     time.sleep(1)
     my_shop.end_turn()
+"""
+    game = Game(my_shop, sample_team, 10, 1, 15)
+    game.print_game()
+    game.move(0, 4)
+    game.print_game()
